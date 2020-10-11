33e17215875e8ead7360f22425d7912be5aed436
Akito <the@akito.ooo>
2020-10-11 18:49:10 +0200

    Show amount of and list of groups

--- tests/whoami.nim
+++ tests/whoami.nim
@@ -1,11 +1,16 @@
 from posix import
   getuid,
   getpwuid,
-  Passwd
+  getgroups,
+  Passwd,
+  Gid
 
+var
+  grouplist: array[0..255, Gid]
 let
   uid = getuid()
   pwd = getpwuid(uid)
+  groupamount = getgroups(255, grouplist.addr)
 
 echo "NAME   :  " & $pwd.pw_name
 echo "UID    :  " & $pwd.pw_uid
@@ -13,3 +18,5 @@ echo "GID    :  " & $pwd.pw_gid
 echo "GECOS  :  " & $pwd.pw_gecos
 echo "DIR    :  " & $pwd.pw_dir
 echo "SHELL  :  " & $pwd.pw_shell
+
+echo "\nGroups : " & " (" & $groupamount & ") " & $grouplist.toOpenArray(0, groupamount - 1)
